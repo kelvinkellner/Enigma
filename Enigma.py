62ee671f1106135e39e876017986c8687f1ddb9f
Kelvin Kellner <kelvinkellner123@gmail.com>
2020-02-08 19:40:06 -0500

    No longer converts message from String > Ints
    This should be more efficient in theory, and it also makes programming the rotors a bit better.
    It also allows me to deal with special characters especially well.

--- Enigma.py
+++ Enigma.py
@@ -1,43 +1,45 @@
 """
-3 wheels, 5 options each, different sizes
+--------------------------------------------------
+Enigma Machine
+--------------------------------------------------
+Built by Kelvin Kellner
+--------------------------------------------------
+Github Project Repo:
+https://github.com/kelvinkellner/Enigma
+--------------------------------------------------
 """
 
-alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+# USER VARIABLES
+switchboard = "AK,BD,EF,GL,PR,CX,WM,QO,IU,YH" # enter swaps in format 'AB,CD,EF..." (the real machine performed 10 swaps)
+
+"""
+--------------------------------------------------
+Inner workings below...
+--------------------------------------------------
+"""
+
+ALPHA = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 # Receive input message
 message = input("Message: ")
-message = message.upper()
+encrypted = message.upper()
 
-# Convert message into integers based on position in the alphabet
-encrypted = []
-for char in message:
-    if char in alpha:
-        encrypted.append(alpha.index(char))
-    else:
-        encrypted.append(char)
-
-# Open switchboard file for character swaps
-txt = open("Switchboard.txt", "r")
-txt = txt.read().split()
+# Create list of individual switchboard swaps
+sb = switchboard.split(",")
 
 # Create tuples for each character swap
 switches = []
-while len(txt) > 0:
-    s = txt.pop(0).split("-")
-    switches.append((alpha.index(s[0]), alpha.index(s[1])))
-
-# Perform first character swap
+while len(sb) > 0:
+    s = sb.pop(0)
+    switches.append((s[0],s[1]))
+    
+# Run first pass through switchboard, swapping letters
 for swap in switches:
     for i in range(len(encrypted)):
         if encrypted[i] == swap[0]:
-            encrypted[i] = swap[1]
+            encrypted = encrypted[:i] + swap[1] + encrypted[i+1:]
         elif encrypted[i] == swap[1]:
-            encrypted[i] = swap[0]
+            encrypted = encrypted[:i] + swap[0] + encrypted[i+1:]
 
-
-
-# Output final encrypted message    
-out = ""
-for i in range(len(encrypted)):
-    out += alpha[encrypted[i]]
-print(message, " -> ", out)
+# Output final encrypted message
+print(message.upper(), " -> ", encrypted)
